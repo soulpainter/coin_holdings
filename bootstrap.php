5ca84a826f48059820096458630157eb19ef02f2
Greg Roberts <soulpainter@gmail.com>
2018-01-18 20:35:52 -0800

    more updates

--- bootstrap.php
+++ bootstrap.php
@@ -2,11 +2,41 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
+setlocale(LC_MONETARY, 'en_US');
+
 define('APP_NAME', 'CoinHolding');
 define('APP_LOG', '/Users/colonel32/Documents/coin_holdings/storage/logs/app.log');
 
+define('TEST_COINLIST_FILE', '/Users/colonel32/Documents/coin_holdings/storage/data/coinlist.json');
+define('TEST_USD_PRICE_FILE', '/Users/colonel32/Documents/coin_holdings/storage/data/USD_price.json');
+
 $log = new Monolog\Logger(APP_NAME);
-$log->pushHandler(new Monolog\Handler\StreamHandler(APP_LOG, Monolog\Logger::WARNING));
+$log->pushHandler(new Monolog\Handler\StreamHandler(APP_LOG, Monolog\Logger::DEBUG));
 # $log->addWarning('Foo');
+
+function getCoinList()
+{
+  $client = new Client([
+    // Base URI is used with relative requests
+    'base_uri' => 'https://www.cryptocompare.com/api/data/',
+    // You can set any number of default request options.
+    'timeout'  => 2.0,
+  ]);
+  $response = $client->request('GET', 'coinlist');
+  $json= $response->getBody();
+  return $json;
+}
+
+function getUSDPriceData($str)
+{
+  $client = new Client([
+    // Base URI is used with relative requests
+    // 'base_uri' => 'https://min-api.cryptocompare.com/data/',
+    // You can set any number of default request options.
+    'timeout'  => 2.0,
+  ]);
+  $response = $client->request('GET', 'https://min-api.cryptocompare.com/data/price?fsym=USD&tsyms=' . $str);
+  $json= $response->getBody();
+}
 
 
--- pull_coinlist.php
+++ pull_coinlist.php
@@ -5,21 +5,35 @@ require_once('bootstrap.php');
 
 use GuzzleHttp\Client;
 
-define('TEST_COINLIST_FILE', '/Users/colonel32/Documents/coin_holdings/storage/data/coinlist.json');
-
 $bittrexHoldings = array('BTC','DASH','DGB','DGC','EMC2',
-                 'EXP','FTP','MAID','MEC','NLG',
-                 'PTC','QRL','THC','XMR');
+                         'EXP','FTP','MAID','MEC','NLG',
+                         'PTC','QRL','THC','XMR');
 
 $coinbaseHoldings = array('BTC','ETH','BCH','LTC');
 
-$krakenHoldings = array('XRP','XMR','MLN','XLM','GNO','ETC','EOS','DOGE','DASH','BCH','BTC','REP');
+$krakenHoldings = array('XRP','XMR','MLN','XLM','GNO','ETC',
+                        'EOS','DOGE','DASH','BCH','BTC','REP');
 
-$jaxxHoldings = array();
+$jaxxHoldings = array(
+  'BTC' => 0.42241173,
+  'BCH' => 1.21864032,
+  'ETH' => 6.83051838,
+  'DASH' => 5,
+  'LTC' => 9.37812472,
+  'ZEC' => 2.80552706,
+  'ETC' => 4.99,
+  'DOGE' => 166411.73,
+  'EOS' => 155.26323,
+  'GNO' => 2.01587,
+  'ICN' => 99.8,
+  'MLN' => 5.83505,
+  'REP' => 22.2352 
+);
 
 $cryptopiaHoldings = array('DBG','NOTE','GAME','NVC','UIS','XVG');
 
-$allHoldings = array_values(array_unique(array_merge($bittrexHoldings, $coinbaseHoldings, $krakenHoldings, $jaxxHoldings, $cryptopiaHoldings)));
+$allHoldings = array_merge($bittrexHoldings, $coinbaseHoldings, $krakenHoldings, $jaxxHoldings, $cryptopiaHoldings);
+$allHoldings = $jaxxHoldings;
 
 if(file_exists(TEST_COINLIST_FILE))
 {
@@ -27,41 +41,42 @@ if(file_exists(TEST_COINLIST_FILE))
 }
 else 
 {
-  $client = new Client([
-    // Base URI is used with relative requests
-    'base_uri' => 'https://www.cryptocompare.com/api/data/',
-    // You can set any number of default request options.
-    'timeout'  => 2.0,
-  ]);
-  $response = $client->request('GET', 'coinlist');
-  $json= $response->getBody();
+  $json = getCoinList();
 }
 
 $coinList = json_decode($json, true);
-#print_r($coinList);
-#print_r($allHoldings);
-#exit;
 
-print implode(',', $allHoldings);
-exit;
+$priceString = implode(',', array_keys($allHoldings));
+
+if(file_exists(TEST_USD_PRICE_FILE))
+{
+  $priceJson = file_get_contents(TEST_USD_PRICE_FILE);
+}
+else
+{
+  $priceJson = getUSDPriceData($priceString);
+}
+
+$usdPrices = json_decode($priceJson, true);
+
+$totalHoldingsValueUSD = 0;
 
 foreach($coinList['Data'] as $symbol=>$coinData)
 {
-  if(in_array($symbol, $allHoldings))
+  if(array_key_exists($symbol, $allHoldings))
   {
-    print "Found $symbol\n";
-    if (($key = array_search($symbol, $allHoldings)) !== false) {
-      unset($allHoldings[$key]);
-    }
+    $usdValue = array_key_exists($symbol,$usdPrices) ? 1/$usdPrices[$symbol] : 0;
+    $totalValue = $usdValue * $allHoldings[$symbol];
+    $totalHoldingsValueUSD += $totalValue;
+    $totalValue = number_format($totalValue, 2);
+    $log->addDebug('Found', ['symbol' => $symbol, 'usd_value' => $usdValue, 'total_coins' => $allHoldings[$symbol], 'total_usd_value' => $totalValue]);
+    unset($allHoldings[$symbol]);
   }
 }
+$log->addDebug('TotalHoldingsValueUSD', ['total_holdings_usd_value' => number_format($totalHoldingsValueUSD, 2)]);
 
 if(count($allHoldings) > 0)
 {
-  $log->addWarning('Coin Symbols Not Found:', $allHoldings);
+  $log->addWarning('NotFound:', $allHoldings);
 }
-
-
-
-
 
